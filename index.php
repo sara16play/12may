810b2d7d3787d4110176138cd5589ae455252840
saurav patel <sauravp651@gmail.com>
2020-05-14 04:21:53 +0530

    last

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ $COLOR = 'blue';
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $color . "<br>";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $color . "<br>";
 ?> 
 
 </body>
