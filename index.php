1bf77d96dc4b484f9cfddeb191869169ac6dbdd6
saurav patel <sauravp651@gmail.com>
2020-05-14 04:40:29 +0530

    lastfrmphp

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php
 $color = "red";
 $COLOR = 'blue';
-echo "My car is " . $color . "<br>";
+echowww "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $color . "<br>";
 echo "My car is " . $color . "<br>";
