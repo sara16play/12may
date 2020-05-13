bc9415735dc3c624c273150bf7edb49f7fbbded5
saurav patel <sauravp651@gmail.com>
2020-05-14 02:06:44 +0530

    secboth

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+$COLOR = 'blue';
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $color . "<br>";
+?> 
+
+</body>
+</html>
