2e82953ee0fa05e4b3a564baecc94f917c13237c
Saurav Patel <42308271+sara16play@users.noreply.github.com>
2020-07-03 12:05:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php
 $color = "red";
 $COLOR = 'blue';
-  echo "THIS IS PHP PAGE......"
+  echo "THIS IS PHP PAGE......";
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $color . "<br>";
